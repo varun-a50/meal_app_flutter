6a2b7977029543080cab1355decf7ac5996159bd
Varun Bhosle <56962637+varun-a50@users.noreply.github.com>
2024-02-05 02:15:20 +0530

    final push

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const TabsScreen() // Todo ...,
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: theme,
+        home: const TabsScreen() // Todo ...,
         );
   }
 }
